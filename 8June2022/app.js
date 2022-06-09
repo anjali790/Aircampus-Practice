32f70e245b7308e9aef0122e6c42f0dee5f904ae
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-09 16:31:09 +0530

    practice

--- 8June2022/app.js
+++ 8June2022/app.js
@@ -232,17 +232,32 @@ if(discriminant >0){
 // Expected Output :
 // Its very hot.
 /* Your answer here
-
-
-
-
-
-
-
-
-
-
 */
+let temp=42
+if(temp<0){
+    console.log("Freezing weather");
+}else if(temp>=0 && temp<10){
+    console.log("Very cold weather");
+}else if(temp>=10 && temp<20){
+    console.log("Cold weather")
+}else if(temp>=20 && temp<30){
+    console.log("Normal in temp");
+}else if(temp>=30 && temp<40){
+    console.log("Its hot");
+}else{
+    console.log("Its very hot");
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 14. Write a program to check whether a triangle is Equilateral, Isosceles or Scalene.

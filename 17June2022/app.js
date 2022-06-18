a0f1f173121fab56455a69927f5b8bf3ccc288de
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-18 12:44:19 +0530

    practice

--- 17June2022/app.js
+++ 17June2022/app.js
@@ -1,18 +1,3 @@
-//Armstrong number
-let num=371;
-let firstDigit=Math.floor(num/100);
-let d=Math.floor(num/10);
-let secondDigit=d%10;
-let thirdDigit=num%10;
-if(((firstDigit**3)+(secondDigit**3)+(thirdDigit**3))==num){
-    console.log("Number is Armstrong number");
-}else{
-    console.log("Number is not Armstrong number");
-}
-
-
-
-
 //Palindrome number
 let num1=292;
 let first_digit=Math.floor(num1/100);

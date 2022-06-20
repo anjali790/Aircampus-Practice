b1e8cb4fb89ee70f27c464b2508f20d7d4abca1b
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-20 18:02:27 +0530

    practice

--- 20June2022/app.js
+++ 20June2022/app.js
@@ -4,7 +4,6 @@
 //11 to 15 days : Rs 4/day
 //After 15 days : Rs 5/day
 function calculateCharge(number_of_days){
-    let charge;
     if(number_of_days<=5){
       return number_of_days*0;
     }else if(number_of_days>=6 && number_of_days<=10){

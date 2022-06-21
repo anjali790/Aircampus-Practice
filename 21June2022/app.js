7bdff71734373890687576c1f568eb489ac87baa
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-21 17:00:03 +0530

    practice

--- 21June2022/app.js
+++ 21June2022/app.js
@@ -35,3 +35,25 @@ console.log(biggestNumber(46,89,98,103))
 
 
 //Take a time in 24 hour fomrat and onvert it to 12 hour format. Also if hour or minute is of single digit add "0" before it
+function amPm(hr,min){
+    if(hr==0){
+      if(min<10) min= '0'+min
+      hr= hr<10? '0'+hr:hr;
+      return ('12:'+min+' AM')
+    }else if(hr>=12){
+      hr=hr-12;
+      min= min<10 ? '0'+min:min;
+      hr= hr<10? '0'+hr:hr;
+      if(hr==0){
+        return ('12:' +min+' PM');
+      }else{
+        return (hr+':'+min+' PM');
+      }
+    }else{
+      min= min<10 ? '0'+min:min;
+      hr= hr<10? '0'+hr:hr;
+      return (hr+':'+min+' AM')
+    }
+  }
+  console.log(amPm(0,25))
+  exports.amPm = amPm

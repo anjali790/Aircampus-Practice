9ed8414ce1c2c6524a04f3befda3e5788e08d1ae
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-23 21:26:35 +0530

    practice

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ let nn=123456789;
 
 var digit = Math.log10(nn)+1;
 console.log(digit);
-digit = parseInt(digit); 
-console.log(digit); // length 
-digit = parseInt(digit /2)
-console.log(digit); // 2 mid 
-digit = Math.pow(10,digit)
-console.log(digit); // power 10 by putting our digit 100
-digit = parseInt(nn / digit)
-console.log(digit); 
-digit = parseInt(digit % 10 )
-console.log(digit); 
+// digit = parseInt(digit); 
+// console.log(digit); // length 
+// digit = parseInt(digit /2)
+// console.log(digit); // 2 mid 
+// digit = Math.pow(10,digit)
+// console.log(digit); // power 10 by putting our digit 100
+// digit = parseInt(nn / digit)
+// console.log(digit); 
+// digit = parseInt(digit % 10 )
+// console.log(digit); 

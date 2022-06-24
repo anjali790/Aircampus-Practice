6c76440ef2d175345b9aa130f481b095bfff4f93
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-24 17:12:44 +0530

    practice

---
+++ 24June2022/app.js
@@ -0,0 +1,67 @@
+//Recursion
+function x(){
+    console.log("hello");
+    return x();
+}
+x();
+
+
+
+
+
+
+
+
+// function y(num){
+//     console.log(num);
+//     return y(num)
+// }
+// y(8);
+
+
+
+
+
+//Print same number
+// function a(num){
+//     if(num>0)
+//         console.log(num);
+//         return a(num-1
+//             );
+// }
+// a(9);
+
+
+
+
+
+
+
+
+
+
+// function z(num){
+//     if(num>0){
+//     console.log(num);
+//     return z(num-1);
+//     }
+// }
+// z(34);
+
+
+
+
+
+
+
+// //factorial
+// function x(num,fact){
+//     if(num>0){
+//         fact=fact*num;
+//     // console.log(fact);
+//     return x(num-1,fact);
+//     }else{
+//         return fact;
+//     }
+// }
+// console.log(x(7,1));

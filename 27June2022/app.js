1f5467a023bae2a141f663fef49728744bccf81f
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-27 16:40:35 +0530

    practice

--- 27June2022/app.js
+++ 27June2022/app.js
@@ -86,7 +86,7 @@ console.log(counter)
 
 
 
-//Find the sum of all digits of a given number
+//Print the sum of all digits of a given number
 let num5=1234,add=0,z;
 while(num5!=0){
     z=num5%10;
@@ -95,4 +95,17 @@ while(num5!=0){
         add+=z;
     }
 }
-console.log(add)
+console.log(add);
+
+
+
+
+
+
+//Print cube of all digits
+let num6=1234, cube=0, digits;
+while(n!=0){
+    digits=num6%10;
+    console.log(digit**3);
+    num6=Math.floor(num6/10);
+}

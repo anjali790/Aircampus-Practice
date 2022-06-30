08ae51e6d8743d159d73c6d8a78c05a7864d9547
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-30 15:22:33 +0530

    practice

--- 30June2022/app.js
+++ 30June2022/app.js
@@ -1,19 +1,10 @@
-//
-let n=4;
-for(let i=1; i<=n; i++){
-    star='';
-    for(let j=1; j<=n-i; j++){
-        star+=" ";
-    }
-    for(let k=0; k<2*i-1; k++){
-        star+="*"
-    }
-    console.log(star)
-}
-
-
-
-
+//print:-
+// 1
+// 01
+// 101
+// 0101
+// 10101
+// 01010
 let num=6;
 for(i=1; i<=num; i++){
   let star=''

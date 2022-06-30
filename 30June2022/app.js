15d7bd0805822e75d2c231e4855d2f2856f75f81
Anjali Yadav <anjaliruby790@gmail.com>
2022-06-30 15:17:29 +0530

    practice

--- 30June2022/app.js
+++ 30June2022/app.js
@@ -1,4 +1,4 @@
-//Pyramid pattern
+//
 let n=4;
 for(let i=1; i<=n; i++){
     star='';
@@ -9,4 +9,22 @@ for(let i=1; i<=n; i++){
         star+="*"
     }
     console.log(star)
+}
+
+
+
+
+let num=6;
+for(i=1; i<=num; i++){
+  let star=''
+  for(j=1; j<=i; j++){
+    let count= 0
+    if((i+j)%2!=0){
+      count=0;
+    }else{
+      count=1;
+    }
+    star=star+count;
+  }
+  console.log(star)
 }

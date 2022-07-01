39e4598312f309c6be0112856f339be1194530bd
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-01 18:01:16 +0530

    practice

--- 1July2022/app.js
+++ 1July2022/app.js
@@ -1,8 +1,31 @@
-console.log("first");
-setTimeout(function(){
-    console.log("hello");
-},5000)
-setTimeout(function(){
-    console.log("calling");
-},2000)
-console.log("second");
+let num=5;
+let count=1;
+for(let i=1; i<=5; i++){
+    let space=" ";
+    let star='';
+    for(let j=1; j<=i; j++){
+        if(count<=9){
+            space="  "
+        }
+        star=star + `${count++} `+ space;
+    }
+    console.log(star)
+}
+
+
+
+
+
+
+
+
+
+
+// console.log("first");
+// setTimeout(function(){
+//     console.log("hello");
+// },5000)
+// setTimeout(function(){
+//     console.log("calling");
+// },2000)
+// console.log("second");

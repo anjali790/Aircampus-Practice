c3113cf389a696e4e47b8602f5e4b7c1d609542b
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-03 21:54:08 +0530

    practice

--- 3July2022/app.js
+++ 3July2022/app.js
@@ -15,3 +15,27 @@ function even(n){
   }
 }
 console.log(even(12067));
+
+
+
+
+
+
+//Take an integer and print 'aircampus' if the input integer is an Aircampus number, otherwise print 'no'.
+//A number is called Aircampus number if the sum of its own digits each raised to the power of the number of digits is equal to the original number itself.
+function sum(n){
+    let temp=n,digit,count,sum=0;
+    while(n!=0){
+        digit=n%10;
+        n=Math.floor(n/10);
+        count++;
+    }
+    n=temp;
+    while(n>0){
+        digit=n%10;
+        n=Math.floor(n/10);
+        sum+=digit**count;
+    }
+    return sum==temp ? "aircampus" : "no";
+}
+console.log(sum(123));

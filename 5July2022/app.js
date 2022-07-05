6680e79bc6b2a3395a5000f1575080a41896a3a8
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-05 17:42:53 +0530

    practice

--- 5July2022/app.js
+++ 5July2022/app.js
@@ -1,5 +1,19 @@
 //-------------------------------------------Array-------------------------------------------------------
 let names=['anjali', 'priya', 'monika', 'nidhi', true]
 console.log(names[0]);
-console.log(names)
-console.log(typeof names)
+console.log(names);
+console.log(typeof names);
+
+
+
+//Using object
+let college={
+    collegeName: 'KEIT',
+    name: 'Anjali',
+    branch: 'EEE',
+    otherBranch: {
+        name: 'Shubro',
+        branch: 'CS'
+    }
+
+}

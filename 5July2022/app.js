1c6b0c466cb75bc74d07aa94a7a98546ded53071
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-05 17:25:24 +0530

    practice

--- 5July2022/app.js
+++ 5July2022/app.js
@@ -1,3 +1,5 @@
 //-------------------------------------------Array-------------------------------------------------------
 let names=['anjali', 'priya', 'monika', 'nidhi']
-console.log(names);
+console.log(names[0]);
+console.log(names)
+console.log(typeof names)

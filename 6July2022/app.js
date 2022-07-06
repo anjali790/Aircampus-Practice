0d514160fe203bd571e5ac03fb09c47d590acb19
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-06 09:10:49 +0530

    practice

---
+++ 6July2022/app.js
@@ -0,0 +1,27 @@
+let nums = [1,2,3,4,5,6,7,8,9]
+
+//print first and last element
+console.log(nums[0],nums[nums.length-1]);
+
+
+
+
+// //PUSH- add element in the last
+// let names = ['david', 'sachin', 'rahul', 'virat']
+// names.push('ishan');
+// console.log(names);
+
+
+
+
+
+// //SHIFT- add element in the first
+// let branches = ['EEE', 'CS', 'IT', 'ME']
+// branches.unshift('EC');
+// console.log(branches);
+
+
+
+
+
+

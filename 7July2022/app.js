1578cdec6ec6fd1c92f8af2b50311ee0d43b1ef9
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-07 18:56:06 +0530

    practice

--- 7July2022/app.js
+++ 7July2022/app.js
@@ -7,3 +7,11 @@ console.log(names.slice(-4));//output:-all names
 console.log(names.slice(0,-2));//output:-Priyanka,Mandavi
 console.log(names.slice(2,5));//output:-starting from index 2 to last value //i.e:- Kritika,Shalini
 console.log(names.slice(-1,-3));//output:-empty array
+
+
+
+
+
+//Includes:-used to check whether the element is present or not in an array
+let nums = [1,2,3,4,5];
+console.log(nums.includes(1));//output:-true

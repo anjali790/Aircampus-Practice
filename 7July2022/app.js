20d1d6d34ff053dd91d9e00977b5fb9c5c88f861
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-07 19:13:16 +0530

    practice

--- 7July2022/app.js
+++ 7July2022/app.js
@@ -20,7 +20,7 @@ console.log(nums.includes(8));//output:-false
 
 //use index and value to search whether the element is present or not in an array
 let numbers = [5,6,9,3,8];
-console.log(numbers.includes(5,0));//true(5 is value and it is at 0 index so output is true)
+console.log(numbers.includes(5,0));//true(value 5 is at 0 index so output is true)
 console.log(numbers.includes(5,3));//false
 
 
@@ -31,10 +31,6 @@ let personNames = ['Divya', 'Ritu', 'Zeenat'];
 console.log([personNames.indexOf('Ritu')]);//index of Ritu is 1
 
 
-//if element is not present in an array then it does not return any index and it return "-1" only 
+//if element is not present in an array then it return "-1" only 
 //consider above example
-console.log(personNames.indexOf('Anjali'));//output:- "-1"(because Anjali is not in an array)
-
-
-
-
+console.log(personNames.indexOf('Anjali'));//output:- "-1"

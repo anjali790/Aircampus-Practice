a7afd830a93b455d733d28bb7d30d15ff1b38afe
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-08 08:04:57 +0530

    practice

--- 8July2022/app.js
+++ 8July2022/app.js
@@ -11,7 +11,7 @@ console.log(max);
 
 
 
-
+//------------------------------------------------------------------------------------------------
 //Minimum number in an array:-
 let numbers = [1,-4,87,-45,0,,-100];
 let min = 0;
@@ -26,9 +26,32 @@ console.log(min);
 
 
 
-
+//----------------------------------------------------------------------------------------------------
 //Index:-
-let num = [1,3,1,6,1,5,8];
-console.log(num.indexOf(1));
+let nums1 = [1,3,1,6,1,5,8];
+console.log(nums1.indexOf(1));
 //Last occurance:-
-console.log(num.lastIndexOf(1));
+console.log(nums1.lastIndexOf(1));
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+//Find in element present at 4th index:-
+let nums2 = [1,2,3,4,5,6];
+console.log(nums2[4]);// 5 is present at 4th index
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+//Find first non repeating:-
+let nums3 = [1,2,5,8,1,7,4,2];
+for(i=0; i<nums3.length; i++){
+    if(nums3.indexOf(nums3[i]) == nums3.lastIndexOf(nums3[i]))
+    console.log(nums3[i]);
+    break;
+}

8f29b987c37dae1370633989d125fbdf71513f68
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-09 20:42:31 +0530

    practice

--- 9July2022/app.js
+++ 9July2022/app.js
@@ -1,6 +1,16 @@
-//Filter:- is used to filter the elements on the basis of speific condition and it does not effect original array, it create a new array and values in it.Filter have inbuild for loop. 
+//Filter:- is used to filter the elements based on a speific condition and it does not effect original array, it create a new array and return values in it.Filter have inbuild for loop. 
 let nums = [1,2,3,4,5,2,4];
 let newArray=nums.filter(function(num){
     return num<4;
 })   
 console.log(newArray);
+
+
+
+
+
+let names=['anjali', 'shubro', 'aarav', 'shubhankar'];
+let array=names.filter(function (name){
+    return name.length>5;
+})
+console.log(array);
